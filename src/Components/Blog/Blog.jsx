d4995aa4d0cdb763c21d7aa2ae96bdc0ff688949
minhaj.dev <minhajhasan.dev@gmail.com>
2024-03-17 02:52:40 +0600

    x

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,6 +3,7 @@ import { BsFillBookmarksFill } from "react-icons/bs";
 
 const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
   const {
+    id,
     title,
     cover,
     author,
@@ -43,7 +44,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
         ))}
       </p>
       <button
-        onClick={() => handleMarkAsRead(reading_time)}
+        onClick={() => handleMarkAsRead(id, reading_time)}
         className="text-purple-600 font-bold underline"
       >
         Mark As Read
